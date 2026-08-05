98a26f354e269bb2c63d644c3d4084ebf96436ac
Marcleônio do Nascimento Medeiros <marcleonio@gmail.com>
2026-08-05 14:10:20 -0300

    Modificando nome da api no swagger

--- backend/src/main/java/com/climaton/climautils/config/OpenApiConfig.java
+++ backend/src/main/java/com/climaton/climautils/config/OpenApiConfig.java
@@ -13,9 +13,9 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("ClimaUtils API - Painel ClimaBrasil")
+                        .title("ClimaUtils API - Painel ClimaSim")
                         .version("1.0.0")
-                        .description("API REST de simulação preditiva e cálculo de trade-offs para o Painel ClimaBrasil usando Regressão OLS.")
+                        .description("API REST de simulação preditiva e cálculo de trade-offs para o Painel ClimaSim usando Regressão OLS.")
                         .contact(new Contact()
                                 .name("Equipe ClimaUtils")
                                 .email("contato@climautils.com")));
